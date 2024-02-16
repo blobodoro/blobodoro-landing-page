f48c256dc86b9dff483a79897f1ace068686bf6a
katmilne <katmilne39@gmail.com>
2024-02-16 11:43:00 +0000

    minor initial edits

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+      <footer className="w-full bg-white text-center py-2 font-[VT323] tracking-wider text-xl">
+        © Copyright 2024 Blobodoro
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ src/components/Layout/Navigation.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image'; 
+
+const Navigation = () => {
+    const router = useRouter();
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // Define the state variable and its setter function
+
+    const handleNavigation = (path: string) => {
+        router.push(path);
+        setIsMenuOpen(false); // Close the menu after clicking on a link
+    };
+
+    const isActive = (path: string) => router.pathname === path;
+
+    return (
+        <header className="bg-zinc-100 shadow-custom text-xl">
+            <nav className="font-[Inter] text-2xl font-bold flex items-center justify-between h-28 w-full ">
+  
+                {/* Render burger icon for small and medium screens */}
+                <div className="flex justify-center md:hidden pr-8">
+                    <Image src="/img/menu.svg" alt="Menu" width={30} height={30} onClick={() => setIsMenuOpen(!isMenuOpen)} className="cursor-pointer" />
+                </div>
+                
+                {/* Render burger menu for small and medium screens */}
+                {isMenuOpen && (
+                    <div className="absolute top-28 left-0 w-full bg-white md:hidden">
+                        <ul className="flex flex-col w-full mx-auto justify-center items-center gap-2">
+                            <li><a className={`hover:text-rose-100 mx-8 transition duration-300 cursor-pointer text-sm ${isActive('/') ? 'text-zinc-400' : ''}`} onClick={() => handleNavigation('/')}>Home</a></li>
+                            <li><a className={`hover:text-rose-100 mx-8 transition duration-300 cursor-pointer text-sm ${isActive('/projects') ? 'text-zinc-400' : ''}`} onClick={() => handleNavigation('/projects')}>Projects</a></li>
+                            <li><a className={`hover:text-rose-100 mx-8 transition duration-300 cursor-pointer text-sm ${isActive('/cv') ? 'text-zinc-400' : ''}`} onClick={() => handleNavigation('/cv')}>CV</a></li>
+                        </ul>
+                    </div>
+                )}
+
+                {/* Render regular navigation links for larger screens */}
+                <div className={`hidden md:flex md:left-0 md:top-[16%] md:w-full ${isMenuOpen ? 'hidden' : 'block'}`}>
+                    <ul className="flex lg:flex-row w-full mx-auto justify-center items-center gap-2">
+                        <li><a className={`hover:text-rose-100 mx-8 transition duration-300 cursor-pointer ${isActive('/') ? 'text-zinc-400' : ''}`} onClick={() => handleNavigation('/')}>Home</a></li>
+                        <li className=""><Image src="/img/logo.png" alt="Pixel Blob Fish" width={100} height={100} className="p-2" /></li>
+                        <li><a className={`hover:text-rose-100 mx-8 transition duration-300 cursor-pointer ${isActive('/projects') ? 'text-zinc-400' : ''}`} onClick={() => handleNavigation('/projects')}>Projects</a></li>
+                        <li><a className={`hover:text-rose-100 mx-8 transition duration-300 cursor-pointer ${isActive('/cv') ? 'text-zinc-400' : ''}`} onClick={() => handleNavigation('/cv')}>CV</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navigation;
